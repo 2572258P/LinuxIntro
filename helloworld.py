c2aca49aba2664089b48117634273d9a1e743d8a
2572258P <2572258P@student.gla.ac.uk>
2021-09-21 20:33:59 +0100

    push.txt

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,20 @@ Spyder Editor
 
 This is a temporary script file.
 """
+__author__ = "jungshic"
+__version__ = "1.0"
+__license__ = "UoG"
+
+import argparse
+from logzero import logger as alias
+
+def main(args):
+    logger.info("hello world")
+    logger.info(args)
+
 print("hello world")
 
+if __name__ == "__main__":
+    print("abc")
     
          
